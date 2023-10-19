ffd2168f3c218e06f403cde5d3826531482d5912
Ben Campbell <ben.campbell.w@gmail.com>
2023-10-18 22:17:04 -0400

    2nd try

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,6 +1,9 @@
-select customer_name, price_per_month, subscription_length FROM
-subscriptions join
-(select orders.subscription_id, customers.customer_name from orders join customers
+select customer_name, sum(total) from
+(select customer_name, price_per_month*subscription_length as total
+ from subscriptions join
+(select customer_name, subscription_id
+from orders join customers
 on orders.customer_id = customers.customer_id
 where orders.order_status = "unpaid") unpaid
-on subscriptions.subscription_id = unpaid.subscription_id;
+on unpaid.subscription_id = subscriptions.subscription_id)
+group by customer_name
