fe01fc1fc742c7a648577f1215169b5bd31754dc
Ben Campbell <ben.campbell.w@gmail.com>
2023-10-18 21:48:09 -0400

    initial try at problem

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,6 @@
--- Add your sql here
+select customer_name, price_per_month, subscription_length FROM
+subscriptions join
+(select orders.subscription_id, customers.customer_name from orders join customers
+on orders.customer_id = customers.customer_id
+where orders.order_status = "unpaid") unpaid
+on subscriptions.subscription_id = unpaid.subscription_id;
